14e791493f7e1799852e60b2b8afd992f06aef8a
VSFerreiras <vsferreiras402@protonmail.com>
2025-09-10 08:37:02 -0400

    Creado jugador list event

---
+++ app/src/main/java/tarea1ap2/ucne/tarea1ap2/presentation/jugadores/list/JugadorListEvent.kt
@@ -0,0 +1,8 @@
+package tarea1ap2.ucne.tarea1ap2.presentation.jugadores.list
+
+sealed interface JugadorListUiEvent {
+    data object Load : JugadorListUiEvent
+    data class Delete(val id: Int) : JugadorListUiEvent
+    data class Edit(val id: Int) : JugadorListUiEvent
+    data object AddNew : JugadorListUiEvent
+}
